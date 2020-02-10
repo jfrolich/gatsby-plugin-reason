10223a30890c053d818df09ec0b5c1551f3f70a6
Jaap Frolich <jfrolich@gmail.com>
2020-02-10 10:28:43 +0800

    improvements

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,31 @@
 const BS_TEST = /\.(bs.js)$/;
 const isCompiledFile = fileName => BS_TEST.test(fileName);
 
+function removeLeadingOrTrailingUnderscores(path) {
+  return path
+    .split("/")
+    .map(p => {
+      p.replace(/^_/, "")
+        .replace(/_$/, "")
+        .replace(/_/g, "-");
+    })
+    .join("/");
+}
+
 exports.onCreatePage = ({ page, actions: { createPage, deletePage } }) => {
   const oldPage = { ...page };
 
   if (isCompiledFile(page.component)) {
     // Remove .bs components so we don't have duplicates
     let { path } = page;
-    let newPath = path
-      .replace(/\.bs/, "")
-      .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2")
-      .toLowerCase();
+    let newPath = removeLeadingOrTrailingUnderscores(
+      path
+        .replace(/\.bs/, "")
+        .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2")
+        .toLowerCase()
+    );
+
+    if (newPath == "/index") newPath = "/";
     deletePage(oldPage);
     createPage({ ...page, path: newPath });
   }
